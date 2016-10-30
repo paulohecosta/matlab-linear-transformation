2ab66fa50eabf5c3ab644758d987d8ad227e93d7
Paulo Henrique da Costa <paulohecosta@gmail.com>
2016-10-30 13:31:57 -0200

    Add files via upload

---
+++ reflexaoY.m
@@ -0,0 +1,22 @@
+function reflexaoY(X, Y, Z, C)
+    newX = X;
+    newY = Y;
+    newZ = Z;
+    
+    canonicaY = [ -1, 0, 0;
+                   0, 1, 0;
+                   0, 0,-1];
+               
+    [xrow,xcol] = size(X);
+    for i = 1:xrow
+        for j = 1:xcol
+            E = [X(i, j); Y(i, j); Z(i, j)];
+            T = canonicaY*E;
+            newX(i, j) = T(1,1);
+            newY(i, j) = T(2,1);
+            newZ(i, j) = T(3,1);
+        end
+    end
+    
+    fill3(newX, newY, newZ, C);
+end
